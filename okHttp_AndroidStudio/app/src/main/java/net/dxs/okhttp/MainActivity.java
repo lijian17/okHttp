517b2f6b5b472240ce03405b54a4d3bbd366a8d4
lijian17 <374452668@qq.com>
2017-07-16 20:52:37 +0800

    文件忽略

--- okHttp_AndroidStudio/app/src/main/java/net/dxs/okhttp/MainActivity.java
+++ okHttp_AndroidStudio/app/src/main/java/net/dxs/okhttp/MainActivity.java
@@ -2,6 +2,9 @@ package net.dxs.okhttp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = (Button) findViewById(R.id.btn_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "你点击了按钮1", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
